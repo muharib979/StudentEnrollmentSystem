2e3f68ac5896b5398327763fa14c1868c70a7922
Muhammad Amran Bhuiyan <35559798+muharib979@users.noreply.github.com>
2018-03-04 09:49:49 +0600

    change

--- resources/views/student/student_setting.blade.php
+++ resources/views/student/student_setting.blade.php
@@ -35,7 +35,7 @@
                 
                   
                              
-                              <button type="submit" class="btn btn-success btn-block">Updated</button>
+                              <button type="submit" class="btn btn-success btn-block">Update</button>
                           </form>
                       </div>
                   </div>
